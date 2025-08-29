d254c5486124b8ad4a1ca8e313a54656e2103018
Teodoro PICCINNI <studio@teodoropiccinni.com>
2025-08-29 11:00:13 +0200

    item_revision: logic refactory

--- erpnext_engineering/engineering/doctype/item_revision/item_revision.py
+++ erpnext_engineering/engineering/doctype/item_revision/item_revision.py
@@ -10,19 +10,15 @@ from frappe.model.document import Document
 
 class ItemRevision(Document):
     def before_insert(self):
-        #check if self.engineering_item_revision_revision is empty
-        if not self.engineering_item_revision_revision:
-            self.engineering_item_revision_revision = 0
-        # check if self.engineering_item_revision_revision exists for self.engineering_item_revision_item_id
-        existing_revision = frappe.get_all("Item Revision", filters={"engineering_item_revision_item_id": self.engineering_item_revision_item_id, "engineering_item_revision_revision": self.engineering_item_revision_revision})
-        if existing_revision:
-            # If it exists, raise an error
-            frappe.throw(_("Item Revision ID {0} already exists").format(self.engineering_item_revision_id))
-        # if not exists generate a new one
-        else:
-            self.engineering_item_revision_id = generate_new_revision_number(self.engineering_item_revision_item_id)
-        # generate generate_engineering_item_revision_id_revision
-        self.engineering_item_revision_item_id_revision = generate_engineering_item_revision_id_revision(self.engineering_item_revision_item_id, self.engineering_item_revision_revision)
+        item_id = self.engineering_item_revision_item_id
+        revision = self.engineering_item_revision_revision
+        # Check and update revision
+        revision = validate_and_update_revision(item_id, revision)
+        # Set engineering_item_revision_revision
+        self.engineering_item_revision_revision = revision
+        # Set Item Code - Revision
+        item_id_revision = generate_engineering_item_revision_id_revision(item_id, revision)
+        self.engineering_item_revision_item_id_revision = item_id_revision
 
 def get_last_item_revision(item_id):
     # get all revision of the selected item and order from greatest to smallest and return the engineering_item_revision_item_id of the first element
@@ -44,15 +40,7 @@ def generate_new_revision_number(item_id):
         return 0
     return last_revision + 1
 
-# Generate engineering_item_revision_item_id_revision from engineering_item_revision_item_id and engineering_item_revision_revision
-def generate_engineering_item_revision_id_revision(engineering_item_revision_item_id, engineering_item_revision_revision):
-    return f"{engineering_item_revision_item_id}-{engineering_item_revision_revision}"
-
-# Set Item revision of Form
-@frappe.whitelist()
-def set_doc_item_revision(doc, method=None):
-    item_id = doc.engineering_item_revision_item_id
-    revision = doc.engineering_item_revision_revision
+def validate_and_update_revision(item_id, revision):
     # Check last revision for the selected Item
     existing_revision = get_last_item_revision(item_id)
     # If it is set, check if it already exists
@@ -66,7 +54,21 @@ def set_doc_item_revision(doc, method=None):
                     revision = existing_revision + 1
     else:
         revision = 0
-    # Set doc.engineering_item_revision_item_id_revision as engineering_item_revision_item_id + "-" + engineering_item_revision_revision
+    return revision
+
+# Generate engineering_item_revision_item_id_revision from engineering_item_revision_item_id and engineering_item_revision_revision
+def generate_engineering_item_revision_id_revision(engineering_item_revision_item_id, engineering_item_revision_revision):
+    return f"{engineering_item_revision_item_id}-rev.{engineering_item_revision_revision}"
+
+# Set Item revision of Form
+@frappe.whitelist()
+def set_doc_item_revision(doc, method=None):
+    item_id = doc.engineering_item_revision_item_id
+    revision = doc.engineering_item_revision_revision
+    # Validate and update revision
+    revision = validate_and_update_revision(item_id, revision)
+    # Set doc.engineering_item_revision_revision
     doc.engineering_item_revision_revision = revision
+    # Set doc.engineering_item_revision_item_id_revision as engineering_item_revision_item_id + "-" + engineering_item_revision_revision
     doc.engineering_item_revision_item_id_revision = generate_engineering_item_revision_id_revision(doc.engineering_item_revision_item_id, doc.engineering_item_revision_revision)
 
