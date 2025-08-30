e047b21fa6543e6a3c777e8f5cf61f183b721852
Teodoro PICCINNI <studio@teodoropiccinni.com>
2025-08-30 10:32:24 +0200

    Item: restrict autocode to new elements

--- erpnext_engineering/engineering/client_script/engineering_script_item_item_coding_table_prefix_check.js
+++ erpnext_engineering/engineering/client_script/engineering_script_item_item_coding_table_prefix_check.js
@@ -1,32 +1,40 @@
 frappe.ui.form.on('Item', {
-    load: function(frm) {
-        alert('Client Script loaded!');
-        console.log('Client Script: Item - loaded');
-    },
+//    load: function(frm) {
+//        if (frm.is_new()) {
+//            alert('Client Script loaded!');
+//            console.log('Client Script: Item - loaded');
+//        }
+//    },
     refresh: function(frm) {
-        console.log('Client Script: Item - refresh');
-        toggle_item_coding_fields(frm);
-        generate_item_coding_code(frm);
+        if (frm.is_new()) {
+            console.log('Client Script: Item - refresh');
+            toggle_item_coding_fields(frm);
+            generate_item_coding_code(frm);
+        }
     },
     engineering_field_item_enable_item_coding_prefix: function(frm) {
-        console.log('Client Script: Item - engineering_field_item_enable_item_coding_prefix');
-        toggle_item_coding_fields(frm);
-        generate_item_coding_code(frm);
+        if (frm.is_new()) {
+            console.log('Client Script: Item - engineering_field_item_enable_item_coding_prefix');
+            toggle_item_coding_fields(frm);
+            generate_item_coding_code(frm);
+        }
     },
     engineering_field_item_item_coding_table_link: function(frm) {
-        console.log('Client Script: Item - engineering_field_item_item_coding_table_link');
-        generate_item_coding_code(frm);
+        if (frm.is_new()) {
+            console.log('Client Script: Item - engineering_field_item_item_coding_table_link');
+            generate_item_coding_code(frm);
+        }
     }
 });
 
-frappe.ui.form.on('Item', 'engineering_field_item_item_coding_table_link', function(frm) {
-    console.log('Client Script: Item - engineering_field_item_item_coding_table_link');
-    generate_item_coding_code(frm);
-});
-frappe.ui.form.on('Item', 'engineering_field_item_item_coding_table_prefix', function(frm) {
-    console.log('Client Script: Item - engineering_field_item_item_coding_table_prefix');
-    generate_item_coding_code(frm);
-});
+//frappe.ui.form.on('Item', 'engineering_field_item_item_coding_table_link', function(frm) {
+//    console.log('Client Script: Item - engineering_field_item_item_coding_table_link');
+//    generate_item_coding_code(frm);
+//});
+//frappe.ui.form.on('Item', 'engineering_field_item_item_coding_table_prefix', function(frm) {
+//    console.log('Client Script: Item - engineering_field_item_item_coding_table_prefix');
+//    generate_item_coding_code(frm);
+//});
 
 function toggle_item_coding_fields(frm) {
     const show_prefix_list = frm.doc.engineering_field_item_enable_item_coding_prefix;
