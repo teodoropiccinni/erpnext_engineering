4b7e836eab1c9cceaa0c44a357f0b40966485b67
Teodoro PICCINNI <studio@teodoropiccinni.com>
2025-09-03 18:01:41 +0200

    Item Coding Table: py update Item field reference engineering_field_item_item_coding_table_prefix

--- erpnext_engineering/engineering/doctype/item_coding_table/item_coding_table.py
+++ erpnext_engineering/engineering/doctype/item_coding_table/item_coding_table.py
@@ -168,7 +168,7 @@ def tpdev_engineering_item_coding_table_get_item_prefix(item_code):
 @frappe.whitelist()
 def tpdev_engineering_doc_item_coding_table_before_insert_item(doc, method=None):
     item_prefix_enabled = doc.engineering_field_item_enable_item_coding_prefix
-    item_prefix = doc.engineering_field_item_coding_prefix
+    item_prefix = doc.engineering_field_item_item_coding_table_prefix
     item_code = doc.item_code
     if doc.isnew():
         # check if code engineering_field_item_enable_item_coding_prefix is enabled
