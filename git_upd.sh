f5726494c4e9445e3298d5dbc569029cbefaa455
Teodoro PICCINNI <studio@teodoropiccinni.com>
2025-09-09 18:22:03 +0200

    git_upd.sh - add more color to outputs

--- git_upd.sh
+++ git_upd.sh
@@ -6,49 +6,49 @@ echo "Execution started at: $(date)"
 echo "Site: $SITE"
 : "${APP_NAME:?Set APP_NAME variable}"
 echo "App Name: $APP_NAME"
-echo "----------------------------------------------"
+echo -e "\033[32m----------------------------------------------\033[0m"
 echo ""
-echo "Updating GIT repository and submodules..."
+echo -e "\033[34mUpdating GIT repository and submodules...\033[0m"
 git pull
 if [ $? -eq 0 ]; then
-  echo "- GIT repo update successful"
+  echo -e "\033[32m- GIT repo update successful\033[0m"
 else
-  echo "- GIT repo update failed"
+  echo -e "\033[31m- GIT repo update failed\033[0m"
   exit 1
 fi
 git pull --recurse-submodules
 if [ $? -eq 0 ]; then
-  echo "- GIT submodule update successful"
+  echo -e "\033[32m- GIT submodule update successful\033[0m"
 else
-  echo "- GIT submodule update failed"
+  echo -e "\033[31m- GIT submodule update failed\033[0m"
   exit 1
 fi
 echo ""
-echo "Checking Frappe Bench version..."
+echo -e "\033[34mChecking Frappe Bench version...\033[0m"
 bench --version
 if [ $? -eq 0 ]; then
-  echo "- Frappe Bench version check successful"
+  echo -e "\033[32m- Frappe Bench version check successful\033[0m"
 else
-  echo "- Frappe Bench version check failed. Is it python available or are you running in the correct virtualenv?"
+  echo -e "\033[31m- Frappe Bench version check failed. Is it python available or are you running in the correct virtualenv?\033[0m"
   exit 1
 fi
 echo ""
-echo "Updating Frappe app: $APP_NAME"
+echo -e "\033[34mUpdating Frappe app: $APP_NAME\033[0m"
 echo "- Uninstalling Frappe app: $APP_NAME"
 bench --site $SITE uninstall-app $APP_NAME
 if [ $? -eq 0 ]; then
-  echo "- - Frappe app: $APP_NAME uninstall successful"
+  echo -e "\033[32m- - Frappe app: $APP_NAME uninstall successful\033[0m"
 else
-  echo "- - Frappe app: $APP_NAME uninstall failed"
+  echo -e "\033[31m- - Frappe app: $APP_NAME uninstall failed\033[0m"
 fi
 echo ""
-echo "Clearing cache..."
+echo -e "\033[34mClearing cache...\033[0m"
 bench --site $SITE clear-cache
 echo ""
 echo "- Installing Frappe app: $APP_NAME"
 bench --site $SITE install-app $APP_NAME
 if [ $? -eq 0 ]; then
-  echo "- - Frappe app: $APP_NAME install successful"
+  echo -e "\033[32m- - Frappe app: $APP_NAME install successful\033[0m"
 else
   echo -e "\033[31m- - Frappe app: $APP_NAME install failed\033[0m"
 fi
